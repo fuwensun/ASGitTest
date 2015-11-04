313767f6b202d2c232c9a5ed8cc0d61846a0270e
wahaha <xxxfuwen@gmail.com>
2015-11-04 15:22:35 +0800

    cm1

---
+++ app/src/main/java/com/example/my/asgittest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.my.asgittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+}
